d97a4f3876289e0c2016ef219208d80a10aba746
Daniel <dangranger17@gmail.com>
2023-06-12 23:01:03 +0100

    Home page start

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -8,8 +8,41 @@
 <style>
     header {
         background-color: #50253D;
+        width: 100vw;
+        max-height: 60px;
     }
-
+    .container, .row {
+        padding: 0px;
+        margin: 0px;
+        max-height: 60px;
+    }
+    #logo {
+        width: 100px;
+        position: relative;
+        top: 14px;
+    }
+    #header-right {
+        display: flex;
+        align-items: center;
+        justify-content: right;
+        max-height: 60px;
+    }
+    #header-right > div {
+        padding-left: 7px;
+        padding-right: 7px;
+    }
+    #cart {
+        height: 20px;
+    }
+    #burger {
+        height: 17px;
+    }
+    .left {
+        margin-right: 5px;
+    }
+    .right {
+        margin-left: 5px;
+    }
     /* Media Queries */
     /* Small */
     @media (min-width: 576px) {
@@ -36,49 +69,19 @@
 </style>
 
 <header>
-    <div class="nav-container">
-        <div class="row">
-            <div class="col-6">1</div>
-            <div class="col-6">2</div>
-        </div>
-    </div>
-</header>
-
-
-<!--<header>
     <div class="container">
         <div class="row">
-            <div class="col-xl-3" id="logo-col">
-                <a href="/">
-                    <img src="/images/header/acai-logo.svg" alt="Acai Logo" id="acai-logo">
-                </a>
+            <div class="col-6">
+                <img src="/images/header/acai-logo.svg" alt="Acai Logo" id="logo">
             </div>
-            <div class="col-xl-6 text-center" id="menu-col">
-                <ul class="nav-menu">
-                    <a href="/shop" class:active={$page.url.pathname === "/shop"}>
-                        <li>SHOP</li>
-                    </a>
-                    <a href="/about-us" class:active={$page.url.pathname === "/about-us"}>
-                        <li>ABOUT US</li>
-                    </a>
-                    <a href="/stores" class:active={$page.url.pathname === "/stores"}>
-                        <li>STORES</li>
-                    </a>
-                    <a href="/blog" class:active={$page.url.pathname === "/blog"}>
-                        <li>BLOG</li>
-                    </a>
-                </ul>
-            </div>
-            <div class="col-xl-3" id="account-col">
-                <div class="icon-container">
-                    <div class="icon-half-1">
-                        <img src="/images/header/cart-icon.svg" alt="Cart Icon">
-                    </div>
-                    <div class="icon-half-2">
-                        <img src="/images/header/account.svg" alt="Icon of a person">
-                    </div>
+            <div class="col-6" id="header-right">
+                <div class="left">
+                    <img src="/images/header/cart-icon.svg" alt="Cart Icon" id="cart">
+                </div>
+                <div class="right">
+                    <img src="/images/header/menu.svg" alt="Menu" id="burger">
                 </div>
             </div>
         </div>
     </div>
-</header>-->
+</header>
