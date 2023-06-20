1f8f6808cb38ed03801efbc8a95e52bdc61a58ac
Toby <toby.king77@gmail.com>
2023-06-20 14:25:36 +0100

    About Us Header + initial content

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -1,1 +1,98 @@
-<h1>About Us Page</h1>
+<style>
+    .header {
+        min-width: 100vw;
+        height: 75vh;
+        background-image: url("/images/about-us/header.png");
+        background-size: cover;
+        background-position: center;
+    }
+    .header-content {
+        background-color: rgba(60, 158, 54, 0.6);
+        color: #fff;
+        text-align: center;
+        width: 54vw;
+        margin: 0 auto;
+        padding: 50px;
+    }
+    .header-content small {
+        font-family: "Now Alt";
+        text-transform: uppercase;
+        font-size: 15px;
+    }
+    .header-content p {
+        font-family: 'Bodoni Moda', serif;
+        font-size: 35px;
+        line-height: 50px;
+    }
+    .page-content {
+        width: 85%;
+        margin: 0 auto;
+        padding-top: 4rem;
+    }
+    .page-content p {
+        font-family: "Now Alt";
+        font-size: 20px;
+        line-height: 50px;
+        font-weight: 400;
+        font-style: normal;
+    }
+    .container {
+        width: 555px;
+        height: 3px;
+        background-color: #3C9E36;
+        position: relative;
+        margin-bottom: 100px;
+        left: 34px;
+    }
+    .circle {
+        display: inline-block;
+        vertical-align: left;
+        width: 15px;
+        height: 15px;
+        background-color: #3C9E36;
+        border: solid 1px #3C9E36;
+        border-radius: 50%;
+        position: absolute;
+        top: -8px;
+        left: -23px;
+    }
+    #about {
+        padding-top: 100px
+    }
+    .box {
+        background: #3C9E36;
+        border: 5px solid #1B6E16;
+        width: 100%;
+        text-align: center;
+        margin-top: 5rem;
+        border-radius: 20px;
+        border-bottom-right-radius: 100px;
+    }
+</style>
+
+<div class="header">
+    <div class="header-content">
+        <small>Our Mission</small>
+        <p>Bringing the incredible benefits of Acai Berries straight from the Amazon Rainforest to your bowl!</p>
+    </div>
+</div>
+<div class="page-content">
+    <div class="row">
+        <div class="col-lg-6" id="about">
+            <div class="container">
+                <div class="circle">
+              
+                </div>
+              </div>
+            <h2>About Us</h2>
+            <p>Founded in 2017 Acai Berry Foods was created with the belief that everybody should be able to eat, enjoy, and benefit from this incredible superfood berry exactly as if they were in the heart of the Amazon Rainforest. We now deliver the freshest and more concentrated Acai product on the market, available to buy as a ready-made bowl from our 7 cafes around London or delivered straight to your door to create your own bowls at home.</p>
+        </div>
+        <div class="col-lg-6">
+            <img src="/images/about-us/shop.png" alt="Acai Berry Shop">
+        </div>
+    </div>
+    <div class="box">
+        <h1>Time To See What All The Fuss Is About!</h1>
+        <p>As the #1 ‘Acai Hotspot’ in London, it’s no surprise that our café’s often have a queue of customers around the building waiting in excitement to get their daily Acai. From delicious freshly made bowls loaded with healthy toppings to energising smoothies, our nutritious Acai Berry products have fast become a crowd favourite. By popular demand, we’re now offering you the ability to create your favourite bowls, smoothies and more at home with our tubs of ready-to-eat frozen Acai!</p>
+    </div>
+</div>
