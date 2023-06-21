204a7aa07a82a32470135db17191fc1b35727a67
Toby <toby.king77@gmail.com>
2023-06-22 00:17:20 +0100

    Terms and Conditions

---
+++ src/routes/terms-conditions/+page.svelte
@@ -0,0 +1,97 @@
+<style>
+    .page-content {
+        max-width: 80vw;
+        margin: 0 auto;
+    }
+    h1 {
+        font-size: 50px;
+        font-family: "Bodoni MT";
+        letter-spacing: -0.75px;
+    }
+    h3 {
+        font-size: 35px;
+        font-family: "Bodoni MT";
+        line-height: 50px;
+    }
+    p, li {
+        font-size: 20px;
+        font-family: "Now Alt";
+        line-height: 50px;
+        margin-bottom: 0;
+    }
+    ul {
+        margin: 0;
+    }
+    @media screen and (max-width: 430px) {
+        p, li {
+            font-size: 12px;
+            line-height: 30px;
+        }
+    }
+</style>
+
+<div class="page-content">
+    <h1>Terms and conditions</h1>
+    
+    <h3>Information about us</h3>
+    <p>Acaiberryfoods.com is a site operated by ABF DIST LIMITED (“We”). We are registered in England and Wales under company number 13499959 and have our registered office at 32 Barn Owl Drive, Bracknell, England, RG12 8FG.</p>
+    
+    <h3>Changes to our Site</h3>
+    <p>We may update our site from time to time and may change the content at any time. However, please note that any of the content on our site may be out of date at any given time, and we are under no obligation to update it.<br>We do not guarantee that our site, or any content on it, will be free from errors or omissions.</p>
+    <h3>Accessing our Site</h3>
+    <p>Our site is made available free of charge</p>
+    <p>We do not guarantee that our site, or any content on it, will always be available or be uninterrupted.</p>
+    <p>Access to our site is permitted on a temporary basis. We may suspend, withdraw, discontinue, or change all or any part of our site without notice. We will not be liable to you if for any reason our site is unavailable at any time or for any period.</p>
+    <p>You are responsible for making all arrangements necessary for you to have access to our site.</p>
+    <p>You are also responsible for ensuring that all persons who access our site through your internet connection are aware of these terms of use and other applicable terms and conditions, and that they comply with them.</p>
+    <h3>Your Account and Password</h3>
+    <p>If you choose, or you are provided with, a user identification code, password, or any other piece of information as part of our security procedures, you must treat such information as confidential. You must not disclose it to any third party.</p>
+    <p>We have the right to disable any user identification code or password, whether chosen by you or allocated by us, at any time, if in our reasonable opinion you have failed to comply with any of the provisions of these terms of use.</p>
+    <p>If you know or suspect that anyone other than you know your user identification code or password, you must promptly notify us here.</p>
+    <h3>Intellectual Property Rights</h3>
+    <p>We are the owner or the licensee of all intellectual property rights in our site, and in the material published on it. Those works are protected by copyright laws and treaties around the world. All such rights are reserved.</p>
+    <p>You may not copy, print, or download extracts of any page(s) from our site for personal or commercial use.</p>
+    <p>If you print off, copy, or download any part of our site in breach of these terms of use, your right to use our site will cease immediately and you must, at our option, return or destroy any copies of the materials you have made.</p>
+    <h3>No reliance on information</h3>
+    <p>The content on our site is provided for general information only. It is not intended to amount to advice on which you should rely. You must obtain professional or specialist advice before taking, or refraining from, any action on the basis of the content on our site.</p>
+    <p>Although we make reasonable efforts to update the information on our site, we make no representations, warranties or guarantees, whether express or implied, that the content on our site is accurate, complete, or up to date.</p>
+    <h3>Limitation of our liability</h3>
+    <p>Nothing in these terms of use excludes or limits our liability for death or personal injury arising from our negligence, or our fraud or fraudulent misrepresentation, or any other liability that cannot be excluded or limited by the law of England and Wales.</p>
+    <p>To the extent permitted by law, we exclude all conditions, warranties, representations, or other terms which may apply to our site or any content on it, whether express or implied.</p>
+    <p>We will not be liable to any user for any loss or damage, whether in contract, tort (including negligence), breach of statutory duty, or otherwise, even if foreseeable, arising under or in connection with:</p>
+    <ul>
+        <li>use of, or inability to use, our site; or</li>
+        <li>use of or reliance on any content displayed on our site.
+        </li>
+    </ul>
+    <p>If you are a business user, please note that, we will not be liable for:</p>
+    <ul>
+        <li>loss of profits, sales, business, or revenue.</li>
+        <li>business interruption.</li>
+        <li>loss of anticipated savings.</li>
+        <li>loss of business opportunity, goodwill, or reputation; or</li>
+        <li>any indirect or consequential loss or damage.</li>
+        <li>If you are a consumer user, please note that we only provide our site for domestic and private use. You agree not to use our site for any commercial or business purposes, and we have no liability to you for any loss of profit, loss of business, business interruption, or loss of business opportunity.</li>
+        <p>We assume no responsibility for the content of websites linked on our site. Such links should not be interpreted as endorsement by us of those linked websites. We will not be liable for any loss or damage that may arise from your use of them.</p>
+    </ul>
+    <h3>Viruses</h3>
+    <p>We do not guarantee that our site will be secure or free from bugs or viruses.</p>
+    <p>You are responsible for configuring your information technology, computer programmed and platform to access our site. You should use your own virus protection software.</p>
+    <p>You must not misuse our site by knowingly introducing viruses, trojans, worms, logic bombs or other material which is malicious or technologically harmful. You must not attempt to gain unauthorized access to our site, the server on which our site is stored, or any server, computer or database connected to our site. You must not attack our site via a denial-of-service attack or a distributed denial-of service attack. By breaching this provision, you would commit a criminal offence under the Computer Misuse.</p>
+    <p>Act 1990. We will report any such breach to the relevant law enforcement authorities, and we will co-operate with those authorities by disclosing your identity to them. In the event of such a breach, your right to use our site will cease immediately.</p>
+    <h3>Linking to our site</h3>
+    <p>You may link to our home page, provided you do so in a way that is fair and legal and does not damage our reputation or take advantage of it</p>
+    <p>You must not establish a link in such a way as to suggest any form of association, approval, or endorsement on our part where none exists.</p>
+    <p>You must not establish a link to our site in any website that is not owned by you.</p>
+    <p>Our site must not be framed on any other site, nor may you create a link to any part of our site other than the home page.</p>
+    <p>We reserve the right to withdraw linking permission without notice.</p>
+    <p>The website in which you are linking must comply in all respects with the content standards set out above.</p>
+    <h3>Applicable Law</h3>
+    <p>If you are a consumer, please note that the terms of use, its subject matter and its formation are governed by the law England and Wales. You and we both agree that the courts of England and Wales will have non-exclusive jurisdiction. However, if you are a resident of Northern Ireland, you may also bring proceedings in Northern Ireland. If you are resident of Scotland, you may also bring proceedings in Scotland.</p>
+    <p>If you are a business, these terms of use, its subject matter, and its formation (and any non-contractual disputes or claims) are governed by the law England and Wales. We both agree to the exclusive jurisdiction of the courts of England and Wales.</p>
+    <h3>Contact Us</h3>
+    <p>Please contact us via email at info@acaiberryfoods.com</p>
+    <p>Thank you for visiting our site.</p>
+
+
+</div>
