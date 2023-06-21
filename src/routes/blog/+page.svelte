f2838154668dffc6d7b50cb6915dd6e6cd31e23a
Toby <toby.king77@gmail.com>
2023-06-22 00:39:53 +0100

    Blog Posts

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,149 @@
+<style>
+     .page-content {
+        width: 85%;
+        margin: 0 auto;
+        margin-bottom: 5rem;
+        padding-top: 4rem;
+    }
+    .page-content p {
+        font-family: "Now Alt";
+        font-size: 20px;
+        line-height: 50px;
+        font-weight: 400;
+        font-style: normal;
+    }
+    .page-content h2 {
+        text-transform: uppercase;
+    }
+    .container {
+        width: 627px;
+        height: 3px;
+        background-color: #3C9E36;
+        position: relative;
+        margin-bottom: 100px;
+        left: 34px;
+    }
+    .circle {
+        display: inline-block;
+        vertical-align: left;
+        width: 15px;
+        height: 15px;
+        background-color: #3C9E36;
+        border: solid 1px #3C9E36;
+        border-radius: 50%;
+        position: absolute;
+        top: -8px;
+        left: -23px;
+    }
+    .container.inverse {
+        left: -85px;
+    }
+    .circle.inverse {
+        left: 101%;
+    }
+    .readmore {
+        text-transform: uppercase;
+        font-size: 13px;
+        font-family: "Now Alt";
+        font-weight: 700;
+        color: #fff;
+        background-color: #3C9E36;
+        border: 2px solid #1B6E16;
+        border-radius: 70px;
+        padding: 1rem 3.5rem;
+        margin: 10px auto;
+    }
+    .readmore:hover {
+        cursor: pointer;
+    }
+    .left, .right {
+        padding-top: 100px
+    }
+    .img-col-left {
+        text-align: right;
+    }
+    .row {
+        margin-bottom: 5rem;
+    }
+</style>
+
+<div class="page-content">
+    <div class="row">
+        <div class="col-lg-6 left">
+            <div class="container">
+                <div class="circle">
+              
+                </div>
+              </div>
+            <h2>ALL ABOUT AÇAÍ</h2>
+            <p>In case you haven’t been bombarded with enough ads and Instagram posts, acai is a superfood berry that is taking the world by storm. But what exactly is acai berry good for and why should you care?</p>
+            <p>In this blog, we’ll give you the low down on nature’s ‘purple gold’, and some uses of acai fruit that can transform your health and wellbeing.</p>
+            <button class="readmore">Read More</button>
+        </div>
+        <div class="col-lg-6 img-col-left">
+            <img src="/images/blog/post-1.png" alt="All About Acai">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <img src="/images/blog/post-2.png" alt="Acai More Than Just a Superfood">
+        </div>
+        <div class="col-lg-6 right">
+            <div class="container inverse">
+                <div class="circle inverse">
+              
+                </div>
+              </div>
+            <h2>AÇAÍ MORE THAN JUST A SUPERFOOD</h2>
+            <p>You may have heard of acai before. It's a superfood that's taken the health industry by storm, and for good reason! Acai berries are packed full of nutrients that have unmatched benefits for your overall health and wellbeing.<br>You might be familiar with acai as a superfood, or as the main ingredient in the trendy acai bowls that are favoured by celebrities around the world. But did you know that there are many different uses for acai?</p>
+            <button class="readmore">Read More</button>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6 left">
+            <div class="container">
+                <div class="circle">
+              
+                </div>
+              </div>
+            <h2>REASONS WHY AÇAÍ IS A SUPERFOOD</h2>
+            <p>Acai berries have been heralded as a superfood for years now. But what exactly makes them so super? And why are they better than other berries, like blueberries or strawberries?</p>
+            <p>In this blog, we’re going to explore the reasons why acai is a superfood, and you may be surprised at some of the health benefits! Let's take a closer look at this amazing fruit and find out what all the fuss is about:</p>
+            <button class="readmore">Read More</button>
+        </div>
+        <div class="col-lg-6 img-col-left">
+            <img src="/images/blog/post-3.png" alt="Reasons Why Acai Is a Superfood">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <img src="/images/blog/post-4.png" alt="Ways you can includ acai into your daily routine">
+        </div>
+        <div class="col-lg-6 right">
+            <div class="container inverse">
+                <div class="circle inverse">
+              
+                </div>
+              </div>
+            <h2>WAYS YOU CAN INCLUDE AÇAÍ INTO YOUR DAILY ROUTINE</h2>
+            <p>Acai is an unlikely superfruit. The acai berry is tiny, jet-black and originates from the swamps of the Amazon. But this little berry has some big health benefits.</p>
+            <p>Acai is rich in antioxidants, fibre, and heart-healthy fats. Including acai in your routine can be easy and delicious! Here are some tips on how to get started.</p>
+            <button class="readmore">Read More</button>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6 left">
+            <div class="container">
+                <div class="circle">
+              
+                </div>
+              </div>
+            <h2>MIRACLE BERRY FOR FITNESS ENTHUSIAST?</h2>
+            <p>We’ve all been there. You’re four weeks into your New Year’s resolution of hitting the gym five times a week and you’re already starting to feel the effects of working out that much. You’re sore, you’re tired, and you just don’t have the energy to keep going. But you don’t want to give up on your resolution so soon! What can you do to give yourself a boost?</p>
+            <button class="readmore">Read More</button>
+        </div>
+        <div class="col-lg-6 img-col-left">
+            <img src="/images/blog/post-5.png" alt="Miracle Berry for Fitness Enthusiast?">
+        </div>
+    </div>
+</div>
